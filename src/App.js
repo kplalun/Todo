a07580809acf276b0ffeefffd25185c256827d73
kplalun <kp.lalun@gmail.com>
2022-12-24 12:21:19 +0700

    complete logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,167 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [todo, setTodo] = useState(() => {
+    const saved = localStorage.getItem("todo");
+    if (saved) {
+      const initialValue = JSON.parse(saved);
+      console.log(initialValue);
+      return initialValue;
+    } else {
+      return [];
+    }
+  });
+
+  const [text, setText] = useState("");
+  const [status, setStatus] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState({});
+
+  function handleInputChange(e) {
+    setCurrentTodo({ ...currentTodo, text: e.target.value });
+    console.log(currentTodo);
+  }
+
+  function getValue(e) {
+    setText(e.target.value);
+  }
+
+  function addTodo() {
+    setTodo([
+      ...todo,
+      {
+        id: todo.length,
+        text: text,
+        status: "",
+      },
+    ]);
+    setText("");
+  }
+
+  function handleCheck(e, index) {
+    const newStatus = [...todo];
+    console.log(newStatus);
+    const status = e.target.checked;
+    newStatus[index].status = status;
+    console.log(newStatus);
+    setTodo(newStatus);
+    setStatus("");
+  }
+
+  function handleEdit(todo) {
+    setEdit(true);
+    setCurrentTodo({ ...todo });
+    console.log(currentTodo);
+  }
+
+  function handleUpdateTodo(index, updateTodo) {
+    console.log(index);
+    console.log(updateTodo);
+    const updateTodoItem = todo.map((item) => {
+      return item.id === index ? updateTodo : item;
+    });
+    setEdit(false);
+    setTodo(updateTodoItem);
+  }
+
+  function handleDelete(index) {
+    const deleteTodo = todo.filter((todo, key) => {
+      return key !== index;
+    });
+    setTodo(deleteTodo);
+    console.log(todo);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("todo", JSON.stringify(todo));
+  }, [todo]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="header">Todo List</h1>
+        {edit ? (
+          <div>
+            <h2>Edit Todo</h2>
+            <label htmlFor="editTodo">EditTodo</label>
+            <input
+              type="text"
+              name="editTodo"
+              placeholder="Edit ToDo"
+              value={currentTodo.text}
+              onChange={handleInputChange}
+            />
+            <button
+              onClick={() => {
+                handleUpdateTodo(currentTodo.id, currentTodo);
+              }}
+            >
+              Update
+            </button>
+            <button
+              onClick={() => {
+                setEdit(false);
+              }}
+            >
+              Cancle
+            </button>
+          </div>
+        ) : (
+          <div>
+            <input
+              type="text"
+              className="inputText"
+              value={text}
+              onChange={(event) => {
+                getValue(event);
+              }}
+            />
+            <button
+              onClick={() => {
+                addTodo();
+              }}
+            >
+              Add
+            </button>
+          </div>
+        )}
+
+        {todo.map((item, index) => {
+          return (
+            <div key={index}>
+              <input
+                type="checkbox"
+                checked={item.status}
+                // defaultChecked={item.status}
+                value={status}
+                onChange={(e) => {
+                  handleCheck(e, index);
+                }}
+              ></input>
+              {/* <h1>No.. {item.id}</h1> */}
+              <h1>
+                {item.text}
+                <button
+                  onClick={() => {
+                    handleEdit(item);
+                  }}
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => {
+                    handleDelete(index);
+                  }}
+                >
+                  X
+                </button>
+              </h1>
+              <hr />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
